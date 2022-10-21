8037fca7cebb5fdfd709a4f174a2d8221fb67e1f
DavidSG-1324 <76829452+DavidSG-1324@users.noreply.github.com>
2022-10-21 14:56:03 -0500

    Cambio header

--- src/componentes/Header.jsx
+++ src/componentes/Header.jsx
@@ -2,7 +2,7 @@ function Header() {
 
 	return (
 		<h1 className="text-center font-black text-5xl mx-auto md:w-2/3">
-			Administrador de Pacientes de {''}<span className="text-indigo-600">Veterinaria</span>
+			APV - {''}<span className="text-indigo-600">Veterinaria</span>
 		</h1>
 	)
 }
